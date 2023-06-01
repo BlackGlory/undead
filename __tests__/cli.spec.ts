62ad1ca0ea3024ae9cc1a968f53041ef8ab3bfe8
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-06-01 12:52:39 +0000

    test: refactor

--- __tests__/cli.spec.ts
+++ __tests__/cli.spec.ts
@@ -1,27 +1,24 @@
-import { exec } from 'child_process'
-import { promisify } from 'util'
+import { Bash } from 'extra-exec'
 import fs from 'fs/promises'
-
-const execAsync = promisify(exec)
 
 describe('cli', () => {
   test('passthrough command and arguments', async () => {
-    const { stdout } = await execAsync('node ./lib/cli.js cat package.json')
+    const result = await Bash.evaluate('node ./lib/cli.js cat package.json')
 
     const expected = await fs.readFile('package.json', 'utf-8')
-    expect(stdout).toBe(expected)
+    expect(result).toBe(expected)
   })
 
   test('inherit environment', async () => {
-    const { stdout } = await execAsync('node ./lib/cli.js echo "$NODE_ENV"')
+    const result = await Bash.evaluate('node ./lib/cli.js echo "$NODE_ENV"')
 
-    expect(stdout).toBe('test\n')
+    expect(result).toBe('test\n')
   })
 
   test('passthrough stdio', async () => {
-    const { stdout } = await execAsync('(node ./lib/cli.js cat) < package.json')
+    const result = await Bash.evaluate('(node ./lib/cli.js cat) < package.json')
 
     const expected = await fs.readFile('package.json', 'utf-8')
-    expect(stdout).toBe(expected)
+    expect(result).toBe(expected)
   })
 })
